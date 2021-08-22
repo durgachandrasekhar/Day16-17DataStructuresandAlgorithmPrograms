bfb552a6a17ac29697d2f0bf8e54c1c6050a1f09
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-22 10:51:03 +0530

    Merge sort

--- DataStructuresandAlgorithmPrograms/Program.cs
+++ DataStructuresandAlgorithmPrograms/Program.cs
@@ -1,40 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace DataStructuresandAlgorithmPrograms
+namespace CSharpMergeSort
 {
-    class BubbleSort
+    class Mergesort
     {
+
+        static public void DoMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[25];
+            int i, left_end, num_elements, tmp_pos;
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+
+            while (left <= left_end)
+                temp[tmp_pos++] = numbers[left++];
+
+            while (mid <= right)
+                temp[tmp_pos++] = numbers[mid++];
+
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        static public void MergeSort_Recursive(int[] numbers, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort_Recursive(numbers, left, mid);
+                MergeSort_Recursive(numbers, (mid + 1), right);
+
+                DoMerge(numbers, left, (mid + 1), right);
+            }
+        }
+
         static void Main(string[] args)
         {
-            int[] a = { 54, 35, 8, 6, 41 };
-            int t;
-            Console.WriteLine("The Array is :");
-            for (int i = 0; i < a.Length; i++)
-            {
-                Console.WriteLine(a[i]);
-            }
+            int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
+            int len = 9;
 
-            for (int j = 0; j < a.Length - 2; j++)
-            {
-                for (int i = 0; i < a.Length - 2; i++)
-                {
-                    if (a[i] > a[i + 1])
-                    {
-                        t = a[i + 1];
-                        a[i + 1] = a[i];
-                        a[i] = t;
-                    }
-                }
-            }
-            Console.WriteLine("The Sorted Array :");
-            foreach (int aray in a)
-                Console.Write(aray + " ");
-            Console.ReadLine();
+            Console.WriteLine("MergeSort By Recursive Method");
+            MergeSort_Recursive(numbers, 0, len - 1);
+            for (int i = 0; i < 9; i++)
+                Console.WriteLine(numbers[i]);
         }
     }
 }
-
-
-
-
-             
