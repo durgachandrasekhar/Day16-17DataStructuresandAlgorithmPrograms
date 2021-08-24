6ed1c4932b8ef7952e28ccb663780d42d24fe5d5
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-24 07:48:52 +0530

    Binary search tree

--- DataStructuresandAlgorithmPrograms/Program.cs
+++ DataStructuresandAlgorithmPrograms/Program.cs
@@ -2,63 +2,42 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CSharpMergeSort
+namespace DataStructuresandAlgorithmPrograms
 {
-    class Mergesort
+    class Binary
+
     {
 
-        static public void DoMerge(int[] numbers, int left, int mid, int right)
+        static public int binarySearch(String[] arr, String x)
         {
-            int[] temp = new int[25];
-            int i, left_end, num_elements, tmp_pos;
-            left_end = (mid - 1);
-            tmp_pos = left;
-            num_elements = (right - left + 1);
+            int num = 0, r = arr.Length - 1;
 
-            while ((left <= left_end) && (mid <= right))
+            while ((num<=r))
             {
-                if (numbers[left] <= numbers[mid])
-                    temp[tmp_pos++] = numbers[left++];
-                else
-                    temp[tmp_pos++] = numbers[mid++];
+                int m = num + (r - num) / 2;
+                int res = x.CompareTo(arr[m]);
+                if (res == 0)
+                    return m;
+                if (res > 0)
+                    num = m + 1;
+                else 
+                    r = m - 1;
             }
+            return -num;
 
-            while (left <= left_end)
-                temp[tmp_pos++] = numbers[left++];
-
-            while (mid <= right)
-                temp[tmp_pos++] = numbers[mid++];
-
-            for (i = 0; i < num_elements; i++)
-            {
-                numbers[right] = temp[right];
-                right--;
-            }
-        }
-
-        static public void MergeSort_Recursive(int[] numbers, int left, int right)
-        {
-            int mid;
-
-            if (right > left)
-            {
-                mid = (right + left) / 2;
-                MergeSort_Recursive(numbers, left, mid);
-                MergeSort_Recursive(numbers, (mid + 1), right);
-
-                DoMerge(numbers, left, (mid + 1), right);
-            }
+           
         }
 
         static void Main(string[] args)
         {
-            int[] numbers = { 3, 8, 7, 5, 2, 1, 9, 6, 4 };
-            int len = 9;
-
-            Console.WriteLine("MergeSort By Recursive Method");
-            MergeSort_Recursive(numbers, 0, len - 1);
-            for (int i = 0; i < 9; i++)
-                Console.WriteLine(numbers[i]);
+            Console.WriteLine("BinarySearch");
+            String[] arr = { "bugatti", "lamborgini", "ferrari", "porsche" };
+            String x = "bugatti";
+            int result = Binary.binarySearch(arr, x);
+            if (result >= 0)
+                Console.WriteLine("The word " + x + " is present at index :" + result);
+            else
+                Console.WriteLine(x + " is not present in list");
         }
     }
 }
