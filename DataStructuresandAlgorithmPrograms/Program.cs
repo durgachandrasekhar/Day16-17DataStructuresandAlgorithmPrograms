7ace79e3884cd6eb0afa53da6202b0d27ce05c43
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-24 08:15:16 +0530

    Banking

--- DataStructuresandAlgorithmPrograms/Program.cs
+++ DataStructuresandAlgorithmPrograms/Program.cs
@@ -1,43 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace DataStructuresandAlgorithmPrograms
+namespace Banking
 {
-    class Binary
-
+    class Program
     {
-
-        static public int binarySearch(String[] arr, String x)
-        {
-            int num = 0, r = arr.Length - 1;
-
-            while ((num<=r))
-            {
-                int m = num + (r - num) / 2;
-                int res = x.CompareTo(arr[m]);
-                if (res == 0)
-                    return m;
-                if (res > 0)
-                    num = m + 1;
-                else 
-                    r = m - 1;
-            }
-            return -num;
-
-           
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine("BinarySearch");
-            String[] arr = { "bugatti", "lamborgini", "ferrari", "porsche" };
-            String x = "bugatti";
-            int result = Binary.binarySearch(arr, x);
-            if (result >= 0)
-                Console.WriteLine("The word " + x + " is present at index :" + result);
-            else
-                Console.WriteLine(x + " is not present in list");
+            Console.WriteLine("Simulate Banking Cash Counter");
+            CashCounter<string> BC1 = new CashCounter<string>();
+            BC1.CashCounter();
         }
     }
 }
